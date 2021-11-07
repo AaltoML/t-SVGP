bac609aa4479e4cb6346e29d5e90eba7df8b37eb
Paul Chang <paul.chang@aalto.fi>
2021-11-07 17:34:33 +0200

    added regression tasks

--- docs/figures/run_iteration_emstyle.py
+++ docs/figures/run_iteration_emstyle.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.model_selection import KFold
 import gpflow
-from src.cvi_svgp import SVGP_CVI
+from src.models.tsvgp import t_SVGP
 from sklearn.preprocessing import StandardScaler
 import time
 from gpflow.optimizers import NaturalGradient
@@ -25,7 +25,7 @@ t_nit = n_e_steps*nit + n_m_steps*nit
 mb_size = 'full'
 n_folds = 5
 
-data_name = 'airfoil' #'boston', 'concrete', 'airfoil'
+data_name = 'airfoil' # Script can run:'boston', 'concrete', 'airfoil'
 optim = 'Adam'
 
 rng = np.random.RandomState(19)
@@ -38,54 +38,38 @@ def init_model(n_train):
 
     # Define standard SVGP
     m = gpflow.models.SVGP(
-        kernel=gpflow.kernels.Matern52(lengthscales=np.ones((1,x.shape[1]))*ell,variance=var),
+        kernel=gpflow.kernels.Matern52(lengthscales=np.ones((1, x.shape[1]))*ell, variance=var),
         likelihood=gpflow.likelihoods.Gaussian(), inducing_variable=Z.copy(), num_data=n_train)
-
 
     models.append(m)
     names.append('svgp')
 
     # Define natgrad SVGP
     m_svgp_nat = gpflow.models.SVGP(
-        kernel=gpflow.kernels.Matern52(lengthscales=np.ones((1,x.shape[1]))*ell,variance=var),
+        kernel=gpflow.kernels.Matern52(lengthscales=np.ones((1, x.shape[1]))*ell, variance=var),
         likelihood=gpflow.likelihoods.Gaussian(), inducing_variable=Z.copy()
-        ,num_data=n_train,whiten=True)
+        , num_data=n_train, whiten=True)
 
     gpflow.set_trainable(m_svgp_nat.q_mu , False)
-    gpflow.set_trainable(m_svgp_nat.q_sqrt , False)
-
+    gpflow.set_trainable(m_svgp_nat.q_sqrt, False)
 
     models.append(m_svgp_nat)
     names.append('svgp_nat')
 
-    # Define non-whitened version
-    m_svgp_nat_nw = gpflow.models.SVGP(
-        kernel=gpflow.kernels.Matern52(lengthscales=np.ones((1,x.shape[1]))*ell,variance=var),
-        likelihood=gpflow.likelihoods.Gaussian(), inducing_variable=Z.copy()
-        ,num_data=n_train,whiten=False)
-
-    gpflow.set_trainable(m_svgp_nat_nw .q_mu , False)
-    gpflow.set_trainable(m_svgp_nat_nw .q_sqrt , False)
-
-
-    models.append(m_svgp_nat_nw)
-    names.append('svgp_nat_nw')
-
-    #Define CVI
-    m_cvi = SVGP_CVI(
-    kernel=gpflow.kernels.Matern52(lengthscales=np.ones((1,x.shape[1]))*ell,variance=var),
+    #Define t_SVGP
+    m_tsvgp = t_SVGP(
+    kernel=gpflow.kernels.Matern52(lengthscales=np.ones((1, x.shape[1]))*ell, variance=var),
     likelihood=gpflow.likelihoods.Gaussian(), inducing_variable=Z.copy()
-    ,num_data=n_train)
-
+    , num_data=n_train)
 
     # Turn off natural params
-    gpflow.set_trainable(m_cvi.lambda_1 , False)
-    gpflow.set_trainable(m_cvi.lambda_2_sqrt , False)
+    gpflow.set_trainable(m_tsvgp.lambda_1, False)
+    gpflow.set_trainable(m_tsvgp.lambda_2_sqrt, False)
 
-    models.append(m_cvi)
-    names.append('svgp_cvi')
+    models.append(m_tsvgp)
+    names.append('tsvgp')
 
-    return models,names
+    return models, names
 
 def run_optim(model, iterations):
     """
@@ -100,6 +84,7 @@ def run_optim(model, iterations):
     nlpd = []
 
     natgrad_opt = NaturalGradient(gamma=nat_lr)
+
     if optim == 'Adam':
         optimizer = tf.optimizers.Adam(adam_lr)
 
@@ -117,79 +102,56 @@ def run_optim(model, iterations):
         natgrad_opt.minimize(training_loss, var_list=variational_params)
 
     @tf.function
-    def optimization_step_cvi(model,training_loss):
+    def optimization_step_tsvgp(model, training_loss):
         model.natgrad_step(*data, lr=nat_lr)
 
     @tf.function
-    def optimization_step(model,training_loss,params):
+    def optimization_step(model, training_loss, params):
         optimizer.minimize(training_loss, var_list=params)
 
     @tf.function
-    def optimization_step2(model,training_loss,params):
+    def optimization_step2(model, training_loss, params):
         optimizer2.minimize(training_loss, var_list=params)
 
     for step in range(iterations):
         data = next(train_iter)
 
-        if model.name=='svgp' and model.q_mu.trainable==False:
+        if model.name == 'svgp' and model.q_mu.trainable == False:
             variational_params = [(model.q_mu, model.q_sqrt)]
 
             for i in range(n_e_steps):
 
-                optimization_step_nat(training_loss,variational_params)
-                # elbo = model.maximum_log_likelihood_objective(data).numpy()
-                # nl = -tf.reduce_mean(model.predict_log_density((xt,yt))).numpy()
-
+                optimization_step_nat(training_loss, variational_params)
 
             elbo = model.maximum_log_likelihood_objective(data).numpy()
             logf.append(elbo)
 
-            nlpd.append(-tf.reduce_mean(model.predict_log_density((xt,yt))).numpy())
-
-
-            # for var in optimizer.variables():
-            #     var.assign(tf.zeros_like(var))
+            nlpd.append(-tf.reduce_mean(model.predict_log_density((xt, yt))).numpy())
 
             for j in range(n_m_steps):
-                optimization_step(model,training_loss,model.trainable_variables)
+                optimization_step(model, training_loss, model.trainable_variables)
 
-
-        elif model.name=='svgp_cvi':
+        elif model.name == 't_svgp':
 
             for i in range(n_e_steps):
-                optimization_step_cvi(model,training_loss)
+                optimization_step_tsvgp(model,training_loss)
 
             elbo = model.maximum_log_likelihood_objective(data).numpy()
             logf.append(elbo)
             nlpd.append(-tf.reduce_mean(model.predict_log_density((xt,yt))).numpy())
 
-            # for var in optimizer.variables():
-            #     var.assign(tf.zeros_like(var))
-
             for i in range(n_m_steps):
-                optimization_step(model,training_loss,model.trainable_variables)
+                optimization_step(model, training_loss, model.trainable_variables)
 
         else:
 
-            # for var in optimizer2.variables():
-            #     var.assign(tf.zeros_like(var))
-
             for i in range(n_e_steps):
-                variational_params =  model.q_mu.trainable_variables + model.q_sqrt.trainable_variables
-                #print(model.trainable_variables)
-                optimization_step2(model,training_loss,variational_params)
-                #elbo = model.maximum_log_likelihood_objective(data).numpy()
-                #nl = -tf.reduce_mean(model.predict_log_density((xt,yt))).numpy()
+                variational_params = model.q_mu.trainable_variables + model.q_sqrt.trainable_variables
+                optimization_step2(model, training_loss, variational_params)
+
             elbo = model.maximum_log_likelihood_objective(data).numpy()
             logf.append(elbo)
             nlpd.append(-tf.reduce_mean(model.predict_log_density((xt,yt))).numpy())
-                #logf.append(elbo)
-                #print(logf)
-                #nlpd.append(nl)
-
-            # for var in optimizer.variables():
-            #     var.assign(tf.zeros_like(var))
-
 
             for i in range(n_m_steps):
                 trainable_variables = model.kernel.trainable_variables + \
@@ -198,17 +160,7 @@ def run_optim(model, iterations):
 
                 optimization_step(model,training_loss,trainable_variables)
 
-
-
-        # elbo = model.maximum_log_likelihood_objective(data).numpy()
-        # logf.append(elbo)
-        #
-        # nlpd.append(-tf.reduce_mean(model.predict_log_density((xt,yt))).numpy())
-
-        #if step % 10 == 0:
-            #print(elbo)
-
-    return logf,nlpd
+    return logf, nlpd
 
 ell = 1.0
 var = 1.0
@@ -219,9 +171,8 @@ if data_name == 'elevators':
     X = data[:, :-1]
     Y = data[:, -1].reshape(-1,1)
 else:
-    data,test = data_load(data_name,split=1.0,normalize=False)
-    X,Y = data
-
+    data, test = data_load(data_name,split=1.0,normalize=False)
+    X, Y = data
 
 X_scaler = StandardScaler().fit(X)
 Y_scaler = StandardScaler().fit(Y)
@@ -231,19 +182,16 @@ N = X.shape[0]
 D = X.shape[1]
 
 # Initialize inducing locations to the first M inputs in the dataset
-
 #kmeans = KMeans(n_clusters=M, random_state=0).fit(X)
 #Z = kmeans.cluster_centers_
 Z = X[:M, :].copy()
 
 kf = KFold(n_splits=n_folds, random_state=0, shuffle=True)
 
-
 RMSE = np.zeros((nm,n_folds))
 ERRP = np.zeros((nm,n_folds))
 NLPD = np.zeros((nm,n_folds))
 TIME = np.zeros((nm,n_folds))
-
 
 NLPD_i = np.zeros((nm,nit,n_folds))
 LOGF_i = np.zeros((nm,nit,n_folds))
@@ -262,87 +210,76 @@ for train_index, test_index in kf.split(X):
 
     train_dataset = tf.data.Dataset.from_tensor_slices((x, y)).repeat().shuffle(x.shape[0])
 
-    mods,names = init_model(x.shape[0])
+    mods, names = init_model(x.shape[0])
 
     maxiter = ci_niter(nit)
 
-    j=0
+    j = 0
 
     for m in mods:
         t0 = time.time()
-        logf_i,nlpd_i = run_optim(m, maxiter)
+        logf_i, nlpd_i = run_optim(m, maxiter)
         t = time.time()-t0
 
-        nlpd = -tf.reduce_mean(m.predict_log_density((xt,yt))).numpy()
-        Eft,_ = m.predict_f(xt,full_output_cov=False)
+        nlpd = -tf.reduce_mean(m.predict_log_density((xt, yt))).numpy()
+        Eft, _ = m.predict_f(xt, full_output_cov=False)
         rmse = tf.math.sqrt(tf.reduce_mean((yt-Eft)**2))
 
-        yp,_ = m.predict_y(xt)
-        errp = 1.-np.sum((yp>0.5)==(yt>0.5))/yt.shape[0]
+        yp, _ = m.predict_y(xt)
+        errp = 1.-np.sum((yp > 0.5) == (yt > 0.5))/yt.shape[0]
 
 
-        print('NLPD for {}: {}'.format(m.name,nlpd))
-        print('ERR% for {}: {}'.format(m.name,rmse))
+        print('NLPD for {}: {}'.format(m.name, nlpd))
+        print('ERR% for {}: {}'.format(m.name, rmse))
 
         # Store results
-        ERRP[j,fold]=rmse
-        NLPD[j,fold]=nlpd
-        TIME[j,fold]=t
+        ERRP[j, fold] = rmse
+        NLPD[j, fold] = nlpd
+        TIME[j, fold] = t
 
-        NLPD_i[j,:,fold] = np.array(nlpd_i)
-        LOGF_i[j,:,fold] = np.array(logf_i)
+        NLPD_i[j, :, fold] = np.array(nlpd_i)
+        LOGF_i[j, :, fold] = np.array(logf_i)
 
-        j+=1
+        j += 1
 
 
-    fold+=1
+    fold += 1
 
 
 # Calculate averages and standard deviations
-rmse_mean = np.mean(ERRP,1)
-rmse_std = np.std(ERRP,1)
-nlpd_mean = np.mean(NLPD,1)
-nlpd_std = np.std(NLPD,1)
-time_mean = np.mean(TIME,1)
-time_std = np.std(TIME,1)
+rmse_mean = np.mean(ERRP, 1)
+rmse_std = np.std(ERRP, 1)
+nlpd_mean = np.mean(NLPD, 1)
+nlpd_std = np.std(NLPD, 1)
+time_mean = np.mean(TIME, 1)
+time_std = np.std(TIME, 1)
 
-elbo_mean = np.mean(LOGF_i,2)
-nlpd_i_mean = np.mean(NLPD_i,2)
+elbo_mean = np.mean(LOGF_i, 2)
+nlpd_i_mean = np.mean(NLPD_i, 2)
 
 plt.title('ELBO'+'_' + data_name)
-plt.plot(range(nit),elbo_mean[0,:][:],label=names[0])
-plt.plot(range(nit),elbo_mean[1,:][:],label=names[1])
-plt.plot(range(nit),elbo_mean[2,:][:],label=names[2])
-plt.plot(range(nit),elbo_mean[3,:][:],label=names[3])
+plt.plot(range(nit), elbo_mean[0, :][:], label=names[0])
+plt.plot(range(nit), elbo_mean[1, :][:], label=names[1])
+plt.plot(range(nit), elbo_mean[2, :][:], label=names[2])
 plt.legend()
 plt.show()
 
 plt.title('NLPD'+'_' + data_name)
-plt.plot(range(nit),nlpd_i_mean[0,:][:],label=names[0])
-plt.plot(range(nit),nlpd_i_mean[1,:][:],label=names[1])
-plt.plot(range(nit),nlpd_i_mean[2,:][:],label=names[2])
-plt.plot(range(nit),nlpd_i_mean[3,:][:],label=names[3])
+plt.plot(range(nit), nlpd_i_mean[0, :][:], label=names[0])
+plt.plot(range(nit), nlpd_i_mean[1, :][:], label=names[1])
+plt.plot(range(nit), nlpd_i_mean[2, :][:], label=names[2])
 plt.legend()
 plt.show()
 
 # Report
-print('Data: {}, n: {}, m: {}, steps: {}'.format(data_name,x.shape[0],mb_size,nit))
-print('{:<14} {:^13}   {:^13}   '.format('Method','NLPD','RMSE'))
+print('Data: {}, n: {}, m: {}, steps: {}'.format(data_name, x.shape[0], mb_size,nit))
+print('{:<14} {:^13}   {:^13}   '.format('Method', 'NLPD', 'RMSE'))
 
 # Report
-print('Data: {}, n: {}, m: {}, steps: {}'.format(data_name,x.shape[0],mb_size,nit))
+print('Data: {}, n: {}, m: {}, steps: {}'.format(data_name, x.shape[0],mb_size,nit))
 print('{:<14} {:^13}   {:^13}   {:^13}'.format('Method','NLPD','RMSE','TIME'))
 for i in range(len(mods)):
     print('{:<14} {:.3f}+/-{:.3f}   {:.3f}+/-{:.3f}   {:.3f}+/-{:.3f}'.format(names[i],
-                                                         nlpd_mean[i],nlpd_std[i],
-                                                         rmse_mean[i],rmse_std[i],
-                                                         time_mean[i],time_std[i]))
-
-# Save results
-np.savez('res/{}-{}-{}-{}.npz'.format(data_name,M,mb_size,nit), data_name=data_name, names=names,
-         minibatch_size=mb_size, maxit=nit, NLPD=NLPD, RMSE=RMSE, TIME=TIME, N=N, D=D,I=M)
-
-# Save results
-np.savez('res/{}-{}-{}-{}-{}-{}-{}-{}.npz'.format(data_name,M,nit,optim,n_e_steps,n_m_steps,nat_lr,adam_lr),
-         data_name=data_name, names=names,
-         minibatch_size=mb_size, maxit=nit, NLPD=NLPD_i, ELBO=LOGF_i, N=N, D=D,I=M)
+                                                         nlpd_mean[i], nlpd_std[i],
+                                                         rmse_mean[i], rmse_std[i],
+                                                         time_mean[i], time_std[i]))
--- src/util.py
+++ src/util.py
@@ -6,7 +6,9 @@ from typing import Optional
 
 import tensorflow as tf
 from gpflow import default_float
-
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 def conditional_from_precision_sites_white(
     Kuu: tf.Tensor,
@@ -436,3 +438,82 @@ def gradient_transformation_mean_var_to_expectation(inputs, grads):
     ▽μ
     """
     return grads[0] - 2.0 * tf.einsum("lmo,ol->ml", grads[1], inputs), grads[1]
+
+
+def data_load(name, split=0.8, seed=0, path='../../docs/notebooks/data/', normalize=False):
+    """
+    Util function to load the UCI datasets
+    """
+    np.random.seed(seed)
+    df = pd.read_csv(r""+path+name+'.csv')
+    if name == 'boston':
+        Xo = df[['crim','zn','indus','chas','nox','rm','age','dis','tax','ptratio','black','lstat']].to_numpy()
+        Yo = df['medv'].to_numpy().reshape((-1,1))
+    elif name == 'concrete':
+        Xo = df[['cement','water','coarse_agg','fine_agg','age']].to_numpy()
+        Yo = df['compressive_strength'].to_numpy().reshape((-1,1))
+    elif name == 'airfoil':
+        Xo = df[['Frequency','AngleAttack','ChordLength','FreeStreamVelocity','SuctionSide']].to_numpy()
+        Yo = df['Sound'].to_numpy().reshape((-1,1))
+    elif name == 'ionosphere':
+        n = np.genfromtxt(r""+path+name+'.csv',delimiter=',')
+        Xo = n[:,:-1]
+        Yo = n[:,-1]
+        Yo = np.reshape(Yo,(-1,1))
+    elif name == 'sonar':
+        n = np.genfromtxt(r""+path+name+'.csv',delimiter=',')
+        Xo = n[:,:-1]
+        Yo = n[:,-1]
+        Yo = np.reshape(Yo,(-1,1))
+    elif name == 'diabetes':
+        Xo = df[['Pregnancies','Glucose','BloodPressure','SkinThickness','Insulin','BMI','DiabetesPedigreeFunction','Age']].to_numpy()
+        Yo = df['Outcome'].to_numpy().reshape((-1,1))
+    elif name == 'german_t':
+        numvars = ['creditamount', 'duration', 'installmentrate', 'residencesince', 'age',
+           'existingcredits', 'peopleliable', 'classification']
+        Xo = df[numvars[:-2]].to_numpy()
+        Yo = df['classification'].to_numpy().reshape((-1,1))
+    elif name == 'german_n':
+        numvars = ['existingchecking', 'duration', 'credithistory', 'purpose', 'creditamount',
+         'savings', 'employmentsince', 'installmentrate', 'statussex', 'otherdebtors',
+         'residencesince', 'property', 'age', 'otherinstallmentplans', 'housing',
+         'existingcredits', 'job', 'peopleliable', 'telephone', 'foreignworker','extra1', 'extra2','extra3','extra4']
+        Xo = df[numvars].to_numpy()
+        Yo = df['classification'].to_numpy().reshape((-1,1))
+    elif name == 'wine':
+        Xo =df[['fixed acidity','volatile acidity', 'citric acid',
+            'residual sugar', 'chlorides', 'free sulfur dioxide',
+            'total sulfur dioxide', 'density', 'pH', 'sulphates','alcohol']].to_numpy()
+        Yo = df['y_value'].to_numpy().reshape((-1,1))
+    elif name == 'naval':
+        Xo = df[['Lever', 'Ship speed', 'Gas Turbine', 'GT rate', 'Gas Generator', 'Starboard Propeller','Port Propeller Torque',
+                'Hight Pressure', 'GT Compressor', 'GT Compressor', 'HP Turbine', 'GT Compressor', 'GT Compressor', 'GT exhaust',
+                'Turbine','Fuel']].to_numpy()
+        Yo = df['y2'].to_numpy().reshape((-1,1))
+    elif name == 'snelson':
+        Xo = df['x'].to_numpy().reshape((-1,1))
+        Yo = df['y'].to_numpy().reshape((-1,1))
+
+    if normalize==True:
+        X_scaler = StandardScaler().fit(Xo)
+        Y_scaler = StandardScaler().fit(Yo)
+        X = X_scaler.transform(Xo)
+        Y = Y_scaler.transform(Yo)
+    else:
+        X = Xo
+        Y = Yo
+
+    perm = np.random.permutation(X.shape[0])
+    np.take(X,perm,axis=0,out=X)
+    np.take(Y,perm,axis=0,out=Y)
+
+    n = Y.shape[0]
+    t = int(np.floor(n*split))
+    x = X[:t,:]
+    y = Y[:t,:]
+    xt = X[t:,:]
+    yt = Y[t:,:]
+    data_train = (x,y)
+    data_test = (xt,yt)
+
+    return data_train, data_test
